61285e5d1a6e9b5cb44af57129d7ecf7b83db7a8
ipbtaliz <federico.picchi@student.unisi.it>
2016-12-13 15:58:24 +0100

    modified argv
    now it uses minimist. I want to add more options.

--- cli.js
+++ cli.js
@@ -1,14 +1,13 @@
 #!/usr/bin/env node
 var mounturl = require('./index.js')
+var argv = require('minimist')(process.argv.slice(2));
 
-var arg = process.argv[2]
-
-if (!arg) {
-  console.error('Usage: mount-url <url>')
+if (argv._.length == 0) {
+  console.error('Usage: mount-url <url> [mount-point] [options]')
   process.exit(1)
 }
 
-mounturl(arg, function (err, cleanup) {
+mounturl(argv, function (err, cleanup) {
   if (err) {
     console.error(err.message)
     process.exit(1)
